3d10822c4a329fd2727ff1c87d288b0fe1b45942
Donovan <donovan@Donovan-MacBook-Pro.local>
2019-11-28 17:41:36 -0800

    added hw5 questions

--- Activities/ClassActivities/src/app/Main.java
+++ Activities/ClassActivities/src/app/Main.java
@@ -1,24 +1,34 @@
 package app;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 
 import ch02.figures.Circle;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("hello world");
-		ArrayList<String> strings = new ArrayList<String>();
-		strings.add("alpha");
-		strings.add("beta");
-		strings.add("gamma");
-		strings.add("delta");
-		Iterator<String> iter = strings.iterator();
-//		String hold;
+//		System.out.println("hello world");
+//		ArrayList<String> strings = new ArrayList<String>();
+//		strings.add("alpha");
+//		strings.add("beta");
+//		strings.add("gamma");
+//		strings.add("delta");
+//		Iterator<String> iter = strings.iterator();
+////		String hold;
+//		
+//		while (iter.hasNext()) {
+//			System.out.println(iter.next());
+//		}
 		
-		while (iter.hasNext()) {
-			System.out.println(iter.next());
-		}
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("Joe", "Shmo");
+		System.out.println(map.toString());
+		if (map.containsKey("Joe"))
+		    System.out.println("yes it does");
+		else
+		    System.out.println("no it does not");
+		
 //		strings.forEach(x -> {
 //			String hold = iter.next();
 //			if (hold.equals("gamma" )) {
